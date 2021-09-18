de5eccea2732ddd488ec73779551ca79848e3928
azamat1696 <azamat1696@gmail.com>
2021-09-18 16:22:57 +0300

    This is a new commit for what I originally planned to be amended

---
+++ cart.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'; ?>
+
+
+<?php 
+// including cart items
+include './Template/_cart-template.php';
+
+// including new phones
+include './Template/_new-phones.php';
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+// session start
+  ob_start();
+// include header
+include 'header.php' ;
+
+?>
+
+
+<?php
+
+// include banner area
+include './Template/_banner.php' ;
+
+// include top sale area
+include './Template/_top-sale.php' ;
+
+// include special price area
+include './Template/_special-price.php' ;
+
+// include banner add area
+include './Template/_banner-add.php' ;
+
+// include new phones area
+include './Template/_new-phones.php' ;
+
+// include Blogs area
+include './Template/_blogs.php' ;
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+<?php
+ // include footer
+ include 'footer.php'; 
+ ?>
